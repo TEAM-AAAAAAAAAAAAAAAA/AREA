6db4ec8f4cda0000ce7cde9bdcccf83819f2868b
Cavonstavant <constant.vigneron@protonmail.com>
2023-01-18 09:54:27 +0100

    db: Add basic prisma configuration along with graphql and apollo setup

---
+++ database/src/schemas/index.ts
@@ -0,0 +1,36 @@
+import { gql } from 'apollo-server';
+import { DateTimeResolver } from 'graphql-scalars';
+import { Context } from '../context';
+
+export const typeDefs = gql`
+    type User {
+        id: ID!
+        name: String!
+        email: [String!]!
+        password: String
+        profile_image: String
+        created_at: DateTime!
+    }
+
+    type Query {
+        user(id: ID!): User
+        allUsers: [User!]!
+    }
+
+    scalar DateTime
+`;
+
+export const resolvers = {
+    Query: {
+        user: async (_: any, args: any, context: Context) => {
+            return await context.prisma.user.findUnique({
+                where: {
+                    id: args.id
+                }
+            });
+        },
+        allUsers: async (_: any, args: any, context: Context) => {
+            return await context.prisma.user.findMany();
+        }
+    }
+};
