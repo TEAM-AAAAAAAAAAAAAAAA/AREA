82b75e47f1496e556e02c5ae426b3cf5263f0c18
majent4 <99695392+majent4@users.noreply.github.com>
2023-01-22 16:53:51 +0100

    npm-build: add capacitor dep

---
+++ client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.teamaaaaaaaaaaaaaaaa.area',
+  appName: 'AREA',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
