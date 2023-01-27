f03e7420ece13efd8d0513fe49b421b60808b53f
hourcadettej <julien.hourcadette@live.fr>
2023-01-27 20:36:39 +0100

    build: bash script docker-start.sh now able to start every service independently of the rest

--- docker-start.sh
+++ docker-start.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 help_list() {
-  echo "Usage :
-  ${0##*/} [-h] [options]
-Options :
-  up :
-    Do docker-compose up -d (Turn up every containers)(-d to execute in background)
-  down :
-    Do docker-compose down (Turn down every containers)
-  apollo :
-    Start apollo container in background
-  web_client :
-    Start web_client container in background
-  mobile_client :
-    Start mobile_client container in background
-  server :
-    Start server container in background
+  echo "[USAGE] :
+  ${0##*/} [-h] [OPTIONS]
+
+[OPTIONS] :
+  up      : Do docker-compose up -d (Turn up every containers)(-d to execute in background)
+  down    : Do docker-compose down (Turn down every containers)
+  serv    : Start server container in background
+  web     : Start web_client container in background
+  mobile  : Start mobile_client container in background
+  db      : Start apollo container in background
   "
 
 }
@@ -29,12 +24,29 @@ parse_options() {
     ;;
 
     up)
-      turn_up
+      all_up
     ;;
 
     down)
-      turn_down
+      all_down
     ;;
+
+    serv)
+      server_up
+    ;;
+
+    web)
+      client_web_up
+    ;;
+
+    mobile)
+      client_mobile_up
+    ;;
+
+    db)
+      apollo_up
+    ;;
+
 
     *)
       echo "Unknown option: ${opt} Run ${0##*/} -h for help.">&2
@@ -42,13 +54,28 @@ parse_options() {
   esac
 }
 
-turn_up() {
-  docker-compose up --build -d
-  docker-compose exec php bash
+all_up() {
+  sudo docker-compose up --build -d
 }
 
-turn_down() {
-  docker-compose down
+all_down() {
+  sudo docker-compose down
+}
+
+server_up() {
+  sudo docker-compose up -d server
+}
+
+client_web_up() {
+  sudo docker-compose up -d client_web
+}
+
+client_mobile_up() {
+  sudo docker-compose up -d client_mobile
+}
+
+apollo_up() {
+  sudo docker-compose up -d apollo
 }
 
 parse_options "$@"
