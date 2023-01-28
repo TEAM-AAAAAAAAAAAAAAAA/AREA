1dfa115e1cafd3e9d48ec3fe9fe900d2830c392d
Broke Machine <emile.ettel@epitech.eu>
2023-01-28 17:16:17 +0100

    back: added mutations to the back, you can now create and add a user, this will not work if two users have the same email or if any of the fields are not filled

--- database/src/schemas/index.ts
+++ database/src/schemas/index.ts
@@ -10,9 +10,26 @@ export const typeDefs = gql`
         created_at: DateTime!
     }
 
+    type Service {
+        id: ID!
+        name: String!
+    }
+
+    type CreateUserResponse {
+        code: Int!
+        success: Boolean!
+        message: String!
+        user: User
+    }
+
     type Query {
         user(id: ID!): User
         allUsers: [User!]!
+        allServices: [Service!]!
+    }
+
+    type Mutation {
+        createUser(name: String!, email: String!, password: String!): CreateUserResponse!
     }
 
     scalar DateTime
@@ -29,6 +46,72 @@ export const resolvers = {
         },
         allUsers: async (_: any, args: any, context: Context) => {
             return await context.prisma.user.findMany();
+        },
+        allServices: async (_: any, args: any, context: Context) => {
+            return await context.prisma.service.findMany();
+        }
+    },
+    Mutation: {
+        createUser: async (_: any, args: any, context: Context) => {
+            if (args.name === undefined || args.name === '') {
+                return await context.prisma.createUserResponse.create({
+                    data: {
+                        code: 400,
+                        success: false,
+                        message: "Name is required"
+                    },
+                });
+            }
+            if (args.email === undefined || args.email === '') {
+                return await context.prisma.createUserResponse.create({
+                    data: {
+                        code: 400,
+                        success: false,
+                        message: "Email is required"
+                    },
+                });
+            }
+            if (args.password === undefined || args.password === '') {
+                return await context.prisma.createUserResponse.create({
+                    data: {
+                        code: 400,
+                        success: false,
+                        message: "Password is required"
+                    },
+                });
+            }
+            if (await context.prisma.user.findUnique({
+                where: {
+                    email: args.email
+                }
+            })) {
+                return await context.prisma.createUserResponse.create({
+                    data: {
+                        code: 400,
+                        success: false,
+                        message: "Email already in use"
+                    },
+                });
+            }
+            const u = await context.prisma.user.create({
+                data: {
+                    name: args.name,
+                    email: args.email,
+                    password: args.password
+                }
+            })
+            return await context.prisma.createUserResponse.create({
+                data: {
+                    code: 200,
+                    success: true,
+                    message: "User created successfully",
+                    user: {
+                        connect: {
+                            id: u.id
+                        }
+                }
+                },
+            });
         }
     }
 };
