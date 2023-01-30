190fbca47d918216e0956079975c3c2505da50ab
Cavonstavant <constant.vigneron@protonmail.com>
2023-01-30 01:55:13 +0100

    auth: Check if user is verified when logging in again

--- server/src/routes/auth/login.ts
+++ server/src/routes/auth/login.ts
@@ -13,67 +13,70 @@ export const login = {
     },
 
     POST: [
-        async (req: Request, res: Response) => {
-        let userInputScheme = Joi.object(({
-            email: Joi.string().email().required(),
-            name: Joi.string().normalize().required(),
-            password: Joi.string().required()
-        }));
-        let userInput = userInputScheme.validate(req.body);
+async (req: Request, res: Response) => {
+let userInputScheme = Joi.object(({
+    email: Joi.string().email().required(),
+    name: Joi.string().normalize().required(),
+    password: Joi.string().required()
+}));
+let userInput = userInputScheme.validate(req.body);
 
-        if (userInput.error !== undefined)
-            return res.status(400).json(userInput.error);
+if (userInput.error !== undefined)
+    return res.status(400).json(userInput.error);
 
-        const user = await prisma.user.findFirst({
-            where: {
+const user = await prisma.user.findFirst({
+    where: {
+        email: userInput.value.email
+    }
+});
+
+if (user) {
+    try {
+        let is_verified = await prisma.user.findUnique({ where: { id: user.id }, select: { emailVerified: true } });
+        if (!is_verified?.emailVerified)
+            return res.status(401).json('Email not verified');
+        if (await argon2id.verify(user.password, userInput.value.password)) {
+            const prismaToken = await prisma.token.findFirst({
+                where: {
+                    userId: user.id,
+                    valid: true,
+                    type: 'API'
+                }
+            });
+            if (prismaToken)
+                (user as any).token = jwt.sign({ token: prismaToken.id }, jwtSecret);
+            res.status(200).json({"status": "ok", "user": _.omit(user, ['password'])});
+        } else {
+            res.status(401).json('Wrong password');
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+} else {
+    try {
+        const password = await argon2id.hash(userInput.value.password);
+        const user = await prisma.user.create({
+            data: {
+                name: userInput.value.name,
                 email: userInput.value.email,
-                emailVerified: true
+                password: password
             }
         });
-
-        if (user) {
-            try {
-                if (await argon2id.verify(user.password, userInput.value.password)) {
-                    const prismaToken = await prisma.token.findFirst({
-                        where: {
-                            userId: user.id,
-                            valid: true,
-                            type: 'API'
-                        }
-                    });
-                    if (prismaToken)
-                        (user as any).token = jwt.sign({ token: prismaToken.id }, jwtSecret);
-                    res.status(200).json({"status": "ok", "user": _.omit(user, ['password'])});
-                } else {
-                    res.status(401).json('Wrong password');
+        const prismaToken = await prisma.token.create({
+            data: {
+                type: 'API',
+                user: {
+                    connect: { id: user.id }
                 }
-            } catch (error) {
-                res.status(500).json(error);
             }
-        } else {
-            try {
-                const password = await argon2id.hash(userInput.value.password);
-                const user = await prisma.user.create({
-                    data: {
-                        name: userInput.value.name,
-                        email: userInput.value.email,
-                        password: password
-                    }
-                });
-                const prismaToken = await prisma.token.create({
-                    data: {
-                        type: 'API',
-                        user: {
-                            connect: { id: user.id }
-                        }
-                    }
-                });
-                (user as any).verification_token = jwt.sign({ token: prismaToken.id }, mailSecret);
-                sendMail(user.email, {token_id: (user as any).verification_token, type_of_action: 'an account verification'});
-                res.status(200).json(user);
-            } catch (error) {
-                res.status(500).json(error);
-            }
-        }
-    }]
+        });
+        (user as any).verification_token = jwt.sign({ token: prismaToken.id }, mailSecret);
+        sendMail(user.email, {token_id: (user as any).verification_token, type_of_action: 'an account verification'});
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+}
+    ]
 };
