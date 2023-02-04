3ba0beefab962213f3be2dabede9927072c8c2ea
aurelenc <aurele.nicolas@epitech.eu>
2023-02-04 23:52:37 +0100

    services: get service and action from prisma

--- server/src/routes/hook/index.ts
+++ server/src/routes/hook/index.ts
@@ -1,38 +1,56 @@
 import { Request, Response } from "express";
-import { reactions } from '../../reactions/.reactions'
 import { services } from "../../services/.services";
 import { transcoders } from "../../transcoders/.transcoders";
 import https from "https";
 import fetch from "node-fetch-commonjs";
 import { prisma } from "../../config/db";
-import { Prisma, Webhook } from "@prisma/client";
+import { Prisma, Service, Webhook } from "@prisma/client";
 
-function getMsgFromHook(hook: string, req: any) {
-    var msg: string = "";
-    if (hook.startsWith("teams")) {
-        msg = req.body.text;
-        msg = msg.substring(msg.indexOf(';') + 1);
-        return msg;
-    } else if (hook.startsWith("discord")) {
-        return req.body.content;
-    } else {
-        return "";
-    }
-}
+// function getMsgFromHook(hook: string, req: any) {
+//     var msg: string = "";
+//     if (hook.startsWith("teams")) {
+//         msg = req.body.text;
+//         msg = msg.substring(msg.indexOf(';') + 1);
+//         return msg;
+//     } else if (hook.startsWith("discord")) {
+//         return req.body.content;
+//     } else {
+//         return "";
+//     }
+// }
 
-function fetchOutgoingWebhook(textKey: string, msg: string, webhookUrl: string) {
-    fetch(webhookUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ [textKey]: msg })
-    });
-}
+// function fetchOutgoingWebhook(textKey: string, msg: string, webhookUrl: string) {
+//     fetch(webhookUrl, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ [textKey]: msg })
+//     });
+// }
 
-function runWebhook(webhook: Webhook | null) : boolean
+// function getService(service: Service) : IService
+// {
+//     return new (table.get(service.name) as new () => services.IService)();
+// }
+
+async function runWebhook(webhook: Webhook) : Promise<boolean>
 {
     console.log(webhook)
+    let action = await prisma.action.findFirst({
+        where: {
+            id: webhook.reactionId
+        }
+    })
+    if (!action)
+        return false;
+    let incomingService = await prisma.service.findFirst({
+        where: {
+            id: action.serviceId
+        }
+    })
+    if (!incomingService)
+        return false;
 
     return true;
 }
@@ -67,7 +85,7 @@ export const hook = {
         if (!webhook)
             return res.status(404).json();
 
-        if (runWebhook(webhook) != true)
+        if (await runWebhook(webhook) != true)
             return res.status(500).json();
 
         return res.status(200).json();
