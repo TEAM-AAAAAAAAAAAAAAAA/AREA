b2cc1a5e00cb444473054cf52d75238428489779
aurelenc <aurele.nicolas@epitech.eu>
2023-02-18 18:16:17 +0100

    format: make discord bot code actually readable

--- discord-bot/src/commands/meeting.ts
+++ discord-bot/src/commands/meeting.ts
@@ -5,46 +5,25 @@ import { prisma } from "../config/db";
 
 @Discord()
 export class Meeting {
-  @Slash({ description: "Meeting" })
-  meeting(
-    @SlashOption({
-      name: "hour",
-      description: "hour of the meeting",
-      required: true,
-      type: ApplicationCommandOptionType.Integer
-    })
-    hour: number,
-    @SlashOption({
-      name: "minute",
-      description: "minute of the meeting",
-      required: false,
-      type: ApplicationCommandOptionType.Integer
-    })
-    minute: number,
-    @SlashOption({
-      name: "month",
-      description: "month of the meeting",
-      required: false,
-      type: ApplicationCommandOptionType.Integer
-    })
-    month: number,
-    @SlashOption({
-      name: "day",
-      description: "day of the meeting",
-      required: false,
-      type: ApplicationCommandOptionType.Integer
-    })
-    day: number,
-    @SlashOption({
-      name: "year",
-      description: "year of the meeting",
-      required: false,
-      type: ApplicationCommandOptionType.Integer
-    })
-    year: number,
-    interaction: CommandInteraction
-  ): void {
-    // prisma.
-    interaction.reply("New meeting at " + hour + ":" + minute || "00" + ", ");
-  }
+    @Slash({ description: "Meeting" })
+    meeting(
+        @SlashOption({name: "hour", description: "hour of the meeting", required: true, type: ApplicationCommandOptionType.Integer})
+        hour: number,
+
+        @SlashOption({ name: "minute", description: "minute of the meeting", required: false, type: ApplicationCommandOptionType.Integer})
+        minute: number,
+
+        @SlashOption({name: "month", description: "month of the meeting", required: false, type: ApplicationCommandOptionType.Integer})
+        month: number,
+
+        @SlashOption({name: "day", description: "day of the meeting", required: false, type: ApplicationCommandOptionType.Integer})
+        day: number,
+
+        @SlashOption({name: "year", description: "year of the meeting", required: false, type: ApplicationCommandOptionType.Integer})
+        year: number,
+
+        interaction: CommandInteraction
+    ): void {
+        interaction.reply("New meeting at " + hour + ":" + minute || "00" + ", ");
+    }
 }
