0a346dcd08b4b5e4f4d50115fe1f91a67731e306
aurelenc <aurele.nicolas@epitech.eu>
2023-02-19 17:47:59 +0100

    discord: async prisma things

--- discord-bot/src/commands/meeting.ts
+++ discord-bot/src/commands/meeting.ts
@@ -7,7 +7,7 @@ import { env } from '../config/env.js';
 @Discord()
 export class Meeting {
     @Slash({ description: "Meeting" })
-    meeting(
+    async meeting(
         @SlashOption({name: "subject", description: "subject of the meeting", required: true, type: ApplicationCommandOptionType.String})
         subject: string,
 
@@ -27,12 +27,12 @@ export class Meeting {
         minute: number,
 
         interaction: CommandInteraction
-    ): void {
-        let webhook = prisma.discordBotWebhook.findUnique({
+    ): Promise<void> {
+        let webhook = await prisma.discordBotWebhook.findUnique({
             where: {
                 command_userId: {
                     command: "meeting",
-                    userId: '211a6457-c335-40c1-b5b1-e40fb4883d59'
+                    userId: 'c3210928-6bc4-407b-805d-39825d59dcc8'
                 }
             },
             select: {
@@ -43,9 +43,9 @@ export class Meeting {
                     }
                 }
             }
-        }).then(hook => {
-            console.log(hook)
-            fetch(env.API_URL + '/hook/' + hook?.webhook.userId + '/' + hook?.webhook.webhookId, {
+        });
+
+        let res = await fetch(env.API_URL + '/hook/' + webhook?.webhook.userId + '/' + webhook?.webhook.webhookId, {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({bot: {
@@ -55,13 +55,12 @@ export class Meeting {
                     day: day,
                     month: month,
                     year: year,
-                    author: hook?.webhook.userId
+                    author: webhook?.webhook.userId
                 }})
-            }).then(res => {
-                console.log(Math.floor(new Date(year, month, day, hour, minute, 0, 0).getTime() / 1000));
-                console.log(res);
-                interaction.reply("Meeting created");
-            })
-        })
+            });
+
+        console.log(Math.floor(new Date(year, month, day, hour, minute, 0, 0).getTime() / 1000));
+        console.log(res);
+        interaction.reply("Meeting created");
     }
 }
