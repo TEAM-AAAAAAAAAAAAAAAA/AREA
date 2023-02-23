cc855709f4523d4f0c25c46a5d77ab6b2274c950
majent4 <99695392+majent4@users.noreply.github.com>
2023-02-23 10:21:52 +0100

    front: remove next from packages

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -2,36 +2,19 @@ import React from 'react';
 import {
   IonContent
 } from '@ionic/react';
-import {
-  useSession
-} from "next-auth/react";
 
 interface ContainerProps {
   name: string;
 }
 
 const LoginContainer: React.FC<ContainerProps> = ({ name }) => {
-  console.log("process.env.NEXTAUTH_URL : " + process.env.NEXTAUTH_URL);
-  console.log("process.env.NEXT_PUBLIC_NEXTAUTH_URL : " + process.env.NEXT_PUBLIC_NEXTAUTH_URL);
-
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <IonContent>
-        <>
-          aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        </>
-      </IonContent>
-    )
-  }
   return (
     <IonContent>
       <>
-      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
       </>
     </IonContent>
-  )
+  );
 };
 
 export default LoginContainer;
