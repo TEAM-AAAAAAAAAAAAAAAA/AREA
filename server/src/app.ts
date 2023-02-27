f511fb1d1ce8156ed99bdc2b8c1691661481b451
Broke Machine <emile.ettel@epitech.eu>
2023-02-27 15:33:59 +0100

    server: added get on 8080/about.json according to the project pdf

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,12 @@ import { Express } from 'express'
 import express from 'express'
 import { routes } from './routes';
 import { env } from './config/env';
-import bodyParser from 'body-parser';import { sendMail } from './utils/mailer';
+import bodyParser from 'body-parser'; import { sendMail } from './utils/mailer';
 import csrf from 'csurf';
 import passport from 'passport';
 import { services } from './services/.services';
+import { Request, Response } from 'express';
+import { prisma } from './config/db';
 
 const app = express();
 
@@ -25,6 +27,44 @@ services.DB.sync().then((res: boolean) => {
 });
 
 app.use('/', routes);
+
+app.get('/about.json', async (req: Request, res: Response) => {
+    let services: any = await prisma.service.findMany().then((res: any) => {
+        return res;
+    });
+    let actions: any = await prisma.action.findMany().then((res: any) => {
+        return res;
+    });
+    let reactions: any = await prisma.reaction.findMany().then((res: any) => {
+        return res;
+    });
+    let aboutServices: [any] = [{}];
+
+    aboutServices.pop()
+    for (let i = 0; i < services.length; i++) {
+        let aboutActions: [any] = [{}];
+        aboutActions.pop()
+        let aboutReactions: [any] = [{}];
+        aboutReactions.pop()
+        for (let j = 0; j < actions.length; j++)
+            if (actions[j].serviceName == services[i].serviceName)
+                aboutActions.push({
+                    name: actions[j].actionName,
+                    description: actions[j].description
+                })
+        for (let j = 0; j < reactions.length; j++)
+            if (reactions[j].serviceName == services[i].serviceName)
+                aboutReactions.push({
+                    description: reactions[j].description
+                })
+        aboutServices.push({
+            name: services[i].serviceName,
+            actions: aboutActions,
+            reactions: aboutReactions
+        })
+    }
+    res.status(200).send(aboutServices);
+});
 
 app.listen(appConfig.port, appConfig.host, () => {
     console.log('[' + appConfig.envName + '] Server is running at ' + appConfig.host + ':' + appConfig.port)
