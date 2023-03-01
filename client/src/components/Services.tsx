734c9cb0a0ccf14e0f424031790c08b07e28a7f8
majent4 <99695392+majent4@users.noreply.github.com>
2023-03-01 13:03:26 +0100

    front: add usequery for services cards

--- client/src/components/Services.tsx
+++ client/src/components/Services.tsx
@@ -7,38 +7,57 @@ import {
     IonCardTitle,
     IonContent
 } from '@ionic/react';
-// import { client } from '../utils/ApolloClient';
-// import { gql } from '@apollo/client';
+import {
+    client
+} from '../utils/ApolloClient';
+import {
+    useQuery,
+    gql,
+    TypedDocumentNode
+} from '@apollo/client';
+
+const GET_ROCKET_INVENTORY = gql`
+  query Query {
+    allServices {
+      serviceName
+    }
+  }
+`;
 
 interface ContainerProps {
     name: string;
 }
 
-// client
-//     .query({
-//         query: gql`
-//       query Query {
-//         allServices {
-//           serviceName
-//         }
-//       }
-//     `,
-//     })
-//     .then((result) => console.log(result));
+client
+    .query({
+        query: gql`
+      query Query {
+        allServices {
+          serviceName
+        }
+      }
+    `,
+    });
 
 const ServicesContainer: React.FC<ContainerProps> = () => {
+    const { loading, data } = useQuery(
+        GET_ROCKET_INVENTORY
+    );
     return (
-        <IonContent>
-            <IonCard>
-                <IonCardHeader>
-                    <IonCardTitle>Service Name</IonCardTitle>
-                    <IonCardSubtitle>Service Type</IonCardSubtitle>
-                </IonCardHeader>
-                <IonCardContent>
-                    Service Description
-                </IonCardContent>
-            </IonCard>
-        </IonContent>
+        { data && data.rocketInventory.map(inventory => (
+            <IonContent>
+                <IonCard>
+                    <IonCardHeader>
+                        <IonCardTitle>{inventory.serviceName}</IonCardTitle>
+                        <IonCardSubtitle>Service Type</IonCardSubtitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                        Service Description
+                    </IonCardContent>
+                </IonCard>
+            </IonContent>
+        ))}
+
     );
 };
 
