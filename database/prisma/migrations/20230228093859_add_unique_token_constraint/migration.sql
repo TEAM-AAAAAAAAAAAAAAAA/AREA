3fa35566f4ef9a0c86438f582a27c76a07fff629
Cavonstavant <constant.vigneron@protonmail.com>
2023-03-01 22:40:14 +0100

    db: reset migrations

--- database/prisma/migrations/20230228093859_add_unique_token_constraint/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,type]` on the table `Token` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Token_userId_type_key" ON "Token"("userId", "type");
