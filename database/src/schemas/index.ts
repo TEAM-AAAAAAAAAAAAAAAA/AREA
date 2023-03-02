3527211a74b029fd709c974b762eec85af629f22
Broke Machine <emile.ettel@epitech.eu>
2023-03-02 10:38:50 +0100

    back: refactored a bunch and fixed discordBotWebhook creation to make it create a new webhook

--- database/src/schemas/index.ts
+++ database/src/schemas/index.ts
@@ -79,6 +79,7 @@ enum TokenType {
         webhookWebhookId: String!
         webhook: Webhook!
         userId: String!
+        serverId: String!
         user: User!
     }
 
@@ -116,7 +117,7 @@ enum TokenType {
         createService(name: String!): Int!
         createChainedReaction(actionId: Int!, reactionName: String!, description: String!, serviceName: String!, actionName: String!, outgoingWebhook: String): Int!
         createOAuthUserData(userId: String!, refreshToken: String, accessToken: String, data: JSONObject, oAuthProviderName: String!, providerUserId: String!): Int!
-        createDiscordBotWebhook(command: String!, webhookWebhookId: String!, userId: String!): Int!
+        createDiscordBotWebhook(command: String!, userId: String!, serverId: String!, reactionName: String!, actionId: String!, serviceId: String!, description: String!, outgoingWebhook: String): Int!
         createToken(userId: String!, type: TokenType!): Token!
     }
 `;
@@ -132,10 +133,10 @@ export const resolvers = {
     },
     DiscordBotWebhook: {
         webhook: async (parent: any, _: any, context: Context) => {
-            return await context.prisma.discordBotWebhook.findUnique({ where: { command_userId: { command: parent.command, userId: parent.userId } } }).webhook();
+            return await context.prisma.discordBotWebhook.findUnique({ where: { command_userId_serverId: { serverId: parent.serverId, command: parent.command, userId: parent.userId } } }).webhook();
         },
         user: async (parent: any, _: any, context: Context) => {
-            return await context.prisma.discordBotWebhook.findUnique({ where: { command_userId: { command: parent.command, userId: parent.userId } } }).user();
+            return await context.prisma.discordBotWebhook.findUnique({ where: { command_userId_serverId: { serverId: parent.serverId, command: parent.command, userId: parent.userId } } }).user();
         }
     },
     oAuthUserData: {
@@ -229,12 +230,6 @@ export const resolvers = {
     },
     Mutation: {
         createToken: async (_: any, args: any, context: Context) => {
-            if (args.type === undefined || args.type === '') {
-                return 400
-            }
-            if (args.userId === undefined || args.userId === '') {
-                return 400
-            }
             await context.prisma.token.create({
                 data: {
                     type: args.type,
@@ -244,34 +239,41 @@ export const resolvers = {
             return 200
         },
         createDiscordBotWebhook: async (_: any, args: any, context: Context) => {
-            if (args.command === undefined || args.command === '') {
-                return 400
-            }
-            if (args.userId === undefined || args.userId === '') {
-                return 400
-            }
-            if (args.webhookWebhookId === undefined || args.webhookId === '') {
-                return 400
-            }
             await context.prisma.discordBotWebhook.create({
                 data: {
                     command: args.command,
-                    userId: args.userId,
-                    webhookWebhookId: args.webhookWebhookId
+                    serverId: args.serverId,
+                    user: {
+                        connect: {
+                            id: args.userId
+                        },
+                    },
+                    webhook: {
+                        create: {
+                            user: {
+                                connect: {
+                                    id: args.userId,
+                                }
+                            },
+                            reaction: {
+                                create: {
+                                    serviceName: args.serviceId,
+                                    reactionName: args.reactionName,
+                                    outgoingWebhook: args.outgoingWebhook,
+                                }
+                            },
+                            incomingService: {
+                                connect: {
+                                    serviceName: args.serviceId,
+                                }
+                            },
+                        }
+                    }
                 }
             });
             return 200
         },
         createOAuthUserData: async (_: any, args: any, context: Context) => {
-            if (args.userId === undefined || args.userId === '') {
-                return 400
-            }
-            if (args.oAuthProviderName === undefined || args.oAuthProviderName === '') {
-                return 400
-            }
-            if (args.providerUserId === undefined || args.providerUserId === '') {
-                return 400
-            }
             const accToken = args.accessToken === undefined ? '' : args.accessToken
             const refToken = args.refreshToken === undefined ? '' : args.refreshToken
             const myData = args.data === undefined ? {} : args.data
@@ -288,29 +290,20 @@ export const resolvers = {
             return 200
         },
         createChainedReaction: async (_: any, args: any, context: Context) => {
-            if (args.actionId === undefined || args.actionId === '') {
-                return 400
-            }
-            if (args.actionName === undefined || args.actionName === '') {
-                return 400
-            }
-            if (args.serviceName === undefined || args.serviceName === '') {
-                return 400
-            }
-            if (args.description === undefined || args.description === '') {
-                return 400
-            }
-            const myHonest = await context.prisma.reaction.create({
-                data: {
-                    reactionName: args.reactionName,
-                    serviceName: args.serviceName,
-                    outgoingWebhook: args.outgoingWebhook,
-                }
-            });
             await context.prisma.actionReaction.create({
                 data: {
-                    actionId: args.actionId,
-                    reactionId: myHonest.reactionId,
+                    action: {
+                        connect: {
+                            reactionId: args.actionId
+                        }
+                    },
+                    reaction: {
+                        create: {
+                            reactionName: args.reactionName,
+                            serviceName: args.serviceName,
+                            outgoingWebhook: args.outgoingWebhook,
+                        }
+                    },
                 }
             });
             return 200
@@ -320,15 +313,6 @@ export const resolvers = {
             return 200;
         },
         createAction: async (_: any, args: any, context: Context) => {
-            if (args.actionName === undefined || args.actionName === '') {
-                return 400
-            }
-            if (args.serviceName === undefined || args.serviceName === '') {
-                return 400
-            }
-            if (args.description === undefined || args.description === '') {
-                return 400
-            }
             await context.prisma.action.create({
                 data: {
                     actionName: args.actionName,
@@ -339,38 +323,30 @@ export const resolvers = {
             return 200
         },
         createWebhook: async (_: any, args: any, context: Context) => {
-            if (args.userId === undefined || args.userId === '') {
-                return 400
-            }
-            if (args.actionId === undefined || args.actionId === '') {
-                return 400
-            }
-            if (args.serviceId === undefined || args.serviceId === '') {
-                return 400
-            }
-            if (args.description === undefined || args.description === '') {
-                return 400
-            }
-            const myHonest = await context.prisma.reaction.create({
-                data: {
-                    serviceName: args.serviceId,
-                    reactionName: args.reactionName,
-                    outgoingWebhook: args.outgoingWebhook,
-                }
-            });
             await context.prisma.webhook.create({
                 data: {
-                    userId: args.userId,
-                    reactionId: myHonest.reactionId,
-                    incomingServiceName: args.serviceId,
+                    user: {
+                        connect: {
+                            id: args.userId,
+                        }
+                    },
+                    reaction: {
+                        create: {
+                            serviceName: args.serviceId,
+                            reactionName: args.reactionName,
+                            outgoingWebhook: args.outgoingWebhook,
+                        }
+                    },
+                    incomingService: {
+                        connect: {
+                            serviceName: args.serviceId,
+                        }
+                    },
                 },
             });
             return 200
         },
         createService: async (_: any, args: any, context: Context) => {
-            if (args.name === undefined || args.name === '') {
-                return 400
-            }
             await context.prisma.service.create({
                 data: {
                     serviceName: args.name
@@ -379,15 +355,6 @@ export const resolvers = {
             return 200
         },
         createUser: async (_: any, args: any, context: Context) => {
-            if (args.name === undefined || args.name === '') {
-                return 400
-            }
-            if (args.email === undefined || args.email === '') {
-                return 400
-            }
-            if (args.password === undefined || args.password === '') {
-                return 400
-            }
             await context.prisma.user.create({
                 data: {
                     name: args.name,
