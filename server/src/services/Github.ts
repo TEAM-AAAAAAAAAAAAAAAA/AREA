48568803e53cab220739f4475cc13fa0a9220e6e
hourcadettej <julien.hourcadette@live.fr>
2023-03-02 17:13:06 +0100

    github: comment source code of Github.ts

--- server/src/services/Github.ts
+++ server/src/services/Github.ts
@@ -24,38 +24,38 @@ export class Github implements IService {
     @area.Action
     @area.Description("Create a new issue")
     async createIssue(): Promise<void> {
-        const octokit = new Octokit({ auth: "TOKEN" });
-        //TODO complete owner, repo, title & body
-        const issueData = {
-            owner: "OWNER",
-            repo: "REPO",
-            title: "TITLE",
-            body: "BODY>",
-        };
-
-        octokit.issues.create(issueData).then((response: { data: { html_url: any; }; }) => {
-            console.log("New issue created: ", response.data.html_url);
-        }).catch((error: any) => {
-            console.error("Error while creating new issue: ", error);
-        });
+        // const octokit = new Octokit({ auth: "TOKEN" });
+        // //TODO complete owner, repo, title & body
+        // const issueData = {
+        //     owner: "OWNER",
+        //     repo: "REPO",
+        //     title: "TITLE",
+        //     body: "BODY>",
+        // };
+        //
+        // octokit.issues.create(issueData).then((response: { data: { html_url: any; }; }) => {
+        //     console.log("New issue created: ", response.data.html_url);
+        // }).catch((error: any) => {
+        //     console.error("Error while creating new issue: ", error);
+        // });
     }
 
     @area.Action
     @area.Description("Get issues' list of a repo")
     async getIssues(): Promise<void> {
-        const octokit = new Octokit({ auth: "TOKEN" });
-        //TODO complete owner, repo, state
-        const issuesData = {
-            owner: "OWNER",
-            repo: "REPO",
-            state: "STATE"
-        };
-
-        octokit.issues.list(issuesData).then((response: { data: { html_url: any; }; }) => {
-            console.log("This is the list of the issues of the repo: ", response.data.html_url);
-        }).catch((error: any) => {
-            console.error("Error while trying to get issues of the repo: ", error);
-        });
+        // const octokit = new Octokit({ auth: "TOKEN" });
+        // //TODO complete owner, repo, state
+        // const issuesData = {
+        //     owner: "OWNER",
+        //     repo: "REPO",
+        //     state: "STATE"
+        // };
+        //
+        // octokit.issues.list(issuesData).then((response: { data: { html_url: any; }; }) => {
+        //     console.log("This is the list of the issues of the repo: ", response.data.html_url);
+        // }).catch((error: any) => {
+        //     console.error("Error while trying to get issues of the repo: ", error);
+        // });
     }
 
     _outgoing: nstring;
