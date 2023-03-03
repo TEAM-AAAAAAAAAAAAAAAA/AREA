d0fe66e9a48d44436f6ed6e10b2e213a48e86a56
hourcadettej <julien.hourcadette@live.fr>
2023-03-03 16:04:39 +0100

    discord-bot: update createIssue command

--- discord-bot/src/commands/createIssue.ts
+++ discord-bot/src/commands/createIssue.ts
@@ -1,8 +1,8 @@
 import { CommandInteraction } from 'discord.js';
 import { ApplicationCommandOptionType } from 'discord.js';
 import { Discord, Slash, SlashOption } from 'discordx';
-import { prisma } from '../config/db.js';
 import { env } from '../config/env.js';
+import {areaConfigCheck} from "../config/areaConfigCheck.js";
 
 @Discord()
 export class CreateIssue {
@@ -23,37 +23,9 @@ export class CreateIssue {
         interaction: CommandInteraction
     ): Promise<void> {
 
-        let thisProvider = await prisma.oAuthProvider.findUnique({
-            where: {
-                serviceName: "Discord"
-            }
-        });
-        if (!thisProvider) {
-            interaction.reply("This AREA server's administrator has not set up the Discord OAuth provider yet");
-            return;
-        }
 
-        let thisUser = await prisma.oAuthUserData.findUnique({
-            where: {
-                providerUserId_oAuthProviderName: {
-                    providerUserId: interaction.user.id,
-                    oAuthProviderName: thisProvider.oAuthProviderName
-                }
-            }
-        });
-        if (!thisUser) {
-            interaction.reply("You need to be linked to an AREA account to use commands");
-            return;
-        }
 
-        let webhook = await prisma.discordBotWebhook.findUnique({
-            where: {
-                command_userId: {
-                    command: "meeting",
-                    userId: thisUser.userId
-                }
-            }
-        });
+        let webhook = await areaConfigCheck(interaction, "issue");
         if (!webhook) {
             interaction.reply("You need to set up a webhook for this command");
             return;
