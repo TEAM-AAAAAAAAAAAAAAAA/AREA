d2f851a52d4d5b6267f4c8ccaf3774f5818212ef
aurelenc <aurele.nicolas@epitech.eu>
2023-03-03 15:52:41 +0100

    about: seed json

--- server/src/app.ts
+++ server/src/app.ts
@@ -32,6 +32,51 @@ app.use(cors());
 
 app.use('/', routes);
 
+async function seedActions()
+{
+    await prisma.action.createMany({
+        data: [
+            {
+                actionName: 'message',
+                serviceName: 'Discord',
+                description: '/message command',
+            },
+            {
+                actionName: 'meeting',
+                serviceName: 'Discord',
+                description: '/meeting command',
+            },
+            {
+                actionName: 'message',
+                serviceName: 'Teams',
+                description: '@AREA Send Message command',
+            },
+            {
+                actionName: 'meeting',
+                serviceName: 'Teams',
+                description: '@AREA Create Meeting command',
+            },
+            {
+                actionName: 'weather_now',
+                serviceName: 'Discord',
+                description: '/weather_now command',
+            },
+            {
+                actionName: 'weather_forecast_hours',
+                serviceName: 'Discord',
+                description: '/weather_forecast_hours command',
+            },
+            {
+                actionName: 'rank',
+                serviceName: 'Discord',
+                description: '/rank command',
+            },
+        ],
+        skipDuplicates: true,
+    });
+}
+
 app.listen(appConfig.port, appConfig.host, () => {
     console.log('[' + appConfig.envName + '] Server is running at ' + appConfig.host + ':' + appConfig.port)
+    seedActions().then(() => console.log("Actions seeded"));
 });
