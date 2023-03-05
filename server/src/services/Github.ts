612218a8205cd25394b4e6fe9e9adfe2657884dc
hourcadettej <julien.hourcadette@live.fr>
2023-03-05 10:28:49 +0100

    server: implementation of action create_issues & get_issues from github

--- server/src/services/Github.ts
+++ server/src/services/Github.ts
@@ -1,9 +1,11 @@
 import { nstring, ustring } from "../types/string";
 import { area } from "../area/.area";
 import { IService } from "./IService";
-const { Octokit } = require("@octokit/rest");
+import { Octokit } from "octokit";
+import {prisma} from "../config/db";
 
 @area.Service
+@area.AuthProvider('github')
 export class Github implements IService {
 
     constructor() {
@@ -24,38 +26,56 @@ export class Github implements IService {
     @area.Action
     @area.Description("Create a new issue")
     async createIssue(): Promise<void> {
-        // const octokit = new Octokit({ auth: "TOKEN" });
-        // //TODO complete owner, repo, title & body
-        // const issueData = {
-        //     owner: "OWNER",
-        //     repo: "REPO",
-        //     title: "TITLE",
-        //     body: "BODY>",
-        // };
-        //
-        // octokit.issues.create(issueData).then((response: { data: { html_url: any; }; }) => {
-        //     console.log("New issue created: ", response.data.html_url);
-        // }).catch((error: any) => {
-        //     console.error("Error while creating new issue: ", error);
-        // });
+        const prismaUserGithub = await prisma.oAuthUserData.findUnique({
+            where: {
+                userId_oAuthProviderName: {
+                    userId: this._userId || '',
+                    oAuthProviderName: "github"
+                }
+            }
+        });
+        if (!prismaUserGithub) return undefined;
+        const octokit = new Octokit({ auth: prismaUserGithub.accessToken });
+        const issueData = {
+            owner: this._owner || '',
+            repo: this._repo || '',
+            title: this._title || '',
+            body: this._body || ''
+        };
+        octokit.rest.issues.create(issueData).then((response: { data: { html_url: any; }; }) => {
+            console.log("New issue created: ", response.data.html_url);
+        }).catch((error: any) => {
+            console.error("Error while creating new issue: ", error);
+        });
     }
 
     @area.Action
     @area.Description("Get issues' list of a repo")
     async getIssues(): Promise<void> {
-        // const octokit = new Octokit({ auth: "TOKEN" });
-        // //TODO complete owner, repo, state
-        // const issuesData = {
-        //     owner: "OWNER",
-        //     repo: "REPO",
-        //     state: "STATE"
-        // };
-        //
-        // octokit.issues.list(issuesData).then((response: { data: { html_url: any; }; }) => {
-        //     console.log("This is the list of the issues of the repo: ", response.data.html_url);
-        // }).catch((error: any) => {
-        //     console.error("Error while trying to get issues of the repo: ", error);
-        // });
+        const prismaUserGithub = await prisma.oAuthUserData.findUnique({
+            where: {
+                userId_oAuthProviderName: {
+                    userId: this._userId || '',
+                    oAuthProviderName: "github"
+                }
+            }
+        });
+        console.log("Devrait y avoir des choses en dessous la");
+        if (!prismaUserGithub) {
+            console.log("Failed to load user");
+            return undefined;
+        }
+        const octokit = new Octokit({ auth: prismaUserGithub.accessToken });
+        const iterator = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
+            owner: this._owner || '',
+            repo: this._repo || '',
+            per_page: 100,
+        });
+        for await (const { data: issues } of iterator) {
+            for (const issue of issues) {
+                console.log("Issue #%d: %s", issue.number, issue.title);
+            }
+        }
     }
 
     _outgoing: nstring;
@@ -63,4 +83,5 @@ export class Github implements IService {
     _owner: ustring;
     _repo: ustring;
     _body: ustring;
+    _userId: ustring;
 }
